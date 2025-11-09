90cc4d1f070b6d75f95d5adcd2a8cf1bf25465bc
Andy <yoongi9289@gmail.com>
2025-11-08 20:12:08 -0700

    finish login modal for both admin and student

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -8,7 +8,8 @@
     const loginMode = ref('student')
 
     const toggleLoginMode = () => {
-      loginMode.value = loginMode.value === 'student' ? 'student' : 'admin';
+      loginMode.value = loginMode.value === 'student' ? 'admin' : 'student';
+      console.log(loginMode.value)
     };
 
 
@@ -46,14 +47,13 @@
           ></v-text-field>    
         </div>
         <div>
-          <h2 style="text-decoration: underline;">Popular Resources</h2>
+          <h2 style="text-decoration: underline;" class="text-center">Popular Resources</h2>
           <v-container class="resources">
               <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
               <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
               <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
           </v-container> 
         </div>
-
       </div>
 
       <!-- Modal -->
@@ -61,41 +61,91 @@
         <v-card style="border-radius: 16px;" class="pa-0 text-center bg-grey-lighten-3">
           <v-container class="ma-0 py-6">
             <v-row no-gutters>
-              <v-col cols="12" md="6">
-                <img src="../assets/student_login.png" width="80%" alt="studing student">              
-                <div class="demoCred">
-                  <h2>Demo credientials:</h2>
-                  <div>
-                    <p><span>ID: </span>student@demo.edu</p>
-                    <p><span>PASS: </span>studentPass123!</p>
+              <template v-if="loginMode === 'student'">
+                <v-col cols="12" md="6">
+                  <img src="../assets/student_login.png" width="80%" alt="studing student">              
+                  <div class="demoCred">
+                    <h2>Demo credientials:</h2>
+                    <div>
+                      <p><span>ID: </span>student@demo.edu</p>
+                      <p><span>PASS: </span>studentPass123!</p>
+                    </div>
                   </div>
-                </div>
-              </v-col>
-              <v-col cols="12" md="6" class="right-side pa-8 d-flex flex-column justify-center ga-3">
-              <!-- Your form content here -->
-                <h2 class="text-h2 mb-4 text-center shrikhand-regular">Student <br/> Login</h2>
-                <v-form class="w-75 align-self-center">
-                  <v-text-field
-                    label="Email"
-                    type="email"
-                    prepend-icon="mdi-email"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="Password"
-                    v-model="password"
-                    :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="visible ? 'text' : 'password'"
-                    @click:append-inner="visible = !visible"
-                    prepend-icon="mdi-lock"
-                    required
-                  ></v-text-field>
-                <v-btn type="submit" color="uni-gold" class="mt-4">Login</v-btn>
-              </v-form>
-              <div class="d-flex justify-center align-center">
-                <p>Login as Admin</p>
-              </div>
-            </v-col>
+                </v-col>
+                <v-col cols="12" md="6" class="right-side pa-8 d-flex flex-column justify-center ga-3">
+                <!-- Your form content here -->
+                  <h2 class="text-h2 mb-4 text-center shrikhand-regular">Student <br/> Login</h2>
+                  <v-form class="w-75 align-self-center">
+                    <v-text-field
+                      label="Email"
+                      type="email"
+                      prepend-icon="mdi-email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="Password"
+                      v-model="password"
+                      :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="visible ? 'text' : 'password'"
+                      @click:append-inner="visible = !visible"
+                      prepend-icon="mdi-lock"
+                      required
+                    ></v-text-field>
+                    <v-btn type="submit" color="uni-gold" class="mt-4">Login</v-btn>
+                  </v-form>
+                  <div class="d-flex justify-center align-center">
+                    <v-btn 
+                    class="mt-3" 
+                    variant="plain" 
+                    color="grey-darken-3"
+                    @click="toggleLoginMode"
+                    >Login as admin</v-btn>
+                  </div>
+                </v-col>
+              </template>
+              <template v-else="loginMode === 'admin'">
+                <v-col cols="12" md="6" class=" pa-8 d-flex flex-column justify-center ga-3">
+                <!-- Your form content here -->
+                  <h2 class="text-h2 mb-4 text-center shrikhand-regular">Admin<br/>Login</h2>
+                  <v-form class="w-75 align-self-center">
+                    <v-text-field
+                      label="Email"
+                      type="email"
+                      prepend-icon="mdi-email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="Password"
+                      v-model="password"
+                      :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="visible ? 'text' : 'password'"
+                      @click:append-inner="visible = !visible"
+                      prepend-icon="mdi-lock"
+                      required
+                    ></v-text-field>
+                    <v-btn type="submit" color="uni-gold" class="mt-4">Login</v-btn>
+                  </v-form>
+                  <div class="d-flex justify-center align-center">
+                    <v-btn 
+                    class="mt-3" 
+                    variant="plain" 
+                    color="grey-darken-3"
+                    @click="toggleLoginMode"
+                    >Login as Student
+                    </v-btn>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <img src="../assets/admin_login.png" width="82%" alt="admin working">              
+                  <div class="demoCred">
+                    <h2>Demo credientials:</h2>
+                    <div>
+                      <p><span>ID: </span>admin@demo.edu</p>
+                      <p><span>PASS: </span>adminPass123!</p>
+                    </div>
+                  </div>
+                </v-col>
+              </template>
             </v-row>
           </v-container>
         </v-card>
