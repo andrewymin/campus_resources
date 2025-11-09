632d6e2ef6bc14b0718fe0a87a7eac2cada87e7b
Andy <yoongi9289@gmail.com>
2025-11-08 18:42:19 -0700

    add comments to each section

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -5,10 +5,18 @@
     const dialog = ref(false)
     const visible = ref(false)
     const password = ref('')
+    const loginMode = ref('student')
+
+    const toggleLoginMode = () => {
+      loginMode.value = loginMode.value === 'student' ? 'student' : 'admin';
+    };
+
+
 </script>
 
 <template>
     <v-main id="home">
+      <!-- Nav Bar -->
         <v-app-bar class="navBar" :elevation="0">
             <v-app-bar-title class="ml-5 ">Campus</v-app-bar-title>
             <v-hover v-slot="{ isHovering, props}">
@@ -23,73 +31,76 @@
             </v-hover>
         </v-app-bar>
 
-    <v-dialog v-model="dialog" max-width="90dvw" >
-       <v-card style="border-radius: 16px;" class="pa-0 text-center bg-grey-lighten-3">
-        <v-container class="ma-0 py-6">
-          <v-row
-            no-gutters
-          >
-            <v-col cols="12" md="6">
-              <img src="../assets/student_login.png" width="80%" alt="studing student">              
-              <div class="demoCred">
-                <h2>Demo credientials:</h2>
-                <div>
-                  <p><span>ID: </span>student@demo.edu</p>
-                  <p><span>PASS: </span>studentPass123!</p>
-                </div>
-              </div>
-            </v-col>
-            <v-col cols="12" md="6" class="right-side pa-8 d-flex flex-column justify-center">
-            <!-- Your form content here -->
-              <h2 class="text-h2 mb-4 text-center shrikhand-regular">Student <br/> Login</h2>
-              <v-form class="w-75 align-self-center">
-                <v-text-field
-                  label="Email"
-                  type="email"
-                  prepend-icon="mdi-email"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="Password"
-                  v-model="password"
-                  :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                  :type="visible ? 'text' : 'password'"
-                  @click:append-inner="visible = !visible"
-                  prepend-icon="mdi-lock"
-                  required
-                ></v-text-field>
-              <v-btn type="submit" color="uni-gold" class="mt-4">Login</v-btn>
-            </v-form>
-            <p>Login as Admin</p>
-          </v-col>
-          </v-row>
-        </v-container>
-       </v-card>
-    </v-dialog>
-
+      <!-- Popular Resources-->
       <div class="d-flex flex-column justifiy-center align-center h-100 ga-16">
         <div class="search">
-              <h1 class="homeTitle">Campus Resources</h1>
-        <v-text-field 
-            class="searchBar"
-            label="Search resources..."
-            prepend-inner-icon="mdi-magnify"
-            variant="solo"
-            density="comfortable"
-            single-line
-            clearable
-      ></v-text-field>    
+          <h1 class="homeTitle">Campus Resources</h1>
+          <v-text-field 
+              class="searchBar"
+              label="Search resources..."
+              prepend-inner-icon="mdi-magnify"
+              variant="solo"
+              density="comfortable"
+              single-line
+              clearable
+          ></v-text-field>    
         </div>
         <div>
-                 <h2 style="text-decoration: underline;">Popular Resources</h2>
-      <v-container class="resources">
-          <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
-          <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
-          <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
-      </v-container> 
+          <h2 style="text-decoration: underline;">Popular Resources</h2>
+          <v-container class="resources">
+              <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
+              <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
+              <v-card title="Counseling Center" text="Mental health services and walk-in counseling" ></v-card>
+          </v-container> 
         </div>
 
-        </div>
+      </div>
+
+      <!-- Modal -->
+      <v-dialog v-model="dialog" max-width="90dvw" >
+        <v-card style="border-radius: 16px;" class="pa-0 text-center bg-grey-lighten-3">
+          <v-container class="ma-0 py-6">
+            <v-row no-gutters>
+              <v-col cols="12" md="6">
+                <img src="../assets/student_login.png" width="80%" alt="studing student">              
+                <div class="demoCred">
+                  <h2>Demo credientials:</h2>
+                  <div>
+                    <p><span>ID: </span>student@demo.edu</p>
+                    <p><span>PASS: </span>studentPass123!</p>
+                  </div>
+                </div>
+              </v-col>
+              <v-col cols="12" md="6" class="right-side pa-8 d-flex flex-column justify-center ga-3">
+              <!-- Your form content here -->
+                <h2 class="text-h2 mb-4 text-center shrikhand-regular">Student <br/> Login</h2>
+                <v-form class="w-75 align-self-center">
+                  <v-text-field
+                    label="Email"
+                    type="email"
+                    prepend-icon="mdi-email"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="visible ? 'text' : 'password'"
+                    @click:append-inner="visible = !visible"
+                    prepend-icon="mdi-lock"
+                    required
+                  ></v-text-field>
+                <v-btn type="submit" color="uni-gold" class="mt-4">Login</v-btn>
+              </v-form>
+              <div class="d-flex justify-center align-center">
+                <p>Login as Admin</p>
+              </div>
+            </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
     </v-main>
 </template>
 
