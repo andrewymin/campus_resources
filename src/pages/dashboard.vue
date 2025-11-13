af0285082509aa8be344fe3707a256f361a3d07f
Andy <yoongi9289@gmail.com>
2025-11-13 14:43:45 -0700

    update side nav of dashboard for mobile

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref } from 'vue';    
+  import { ref, shallowRef } from 'vue';    
   import { useRouter } from 'vue-router';
   import data from '../data/data.json';
   import { useDisplay } from 'vuetify';
@@ -20,6 +20,9 @@
     { id: 3, title: 'Item 3', text: 'This is the third saved item.' },
   ]);
 
+  const adminMenuTitles = ['Resources', 'Annoucements', 'Users'];
+  const adminMenuModel = shallowRef('Resources');
+
   const goHome = () => {
       router.push('/'); 
   }
@@ -32,16 +35,65 @@
 
 <template>
   <v-container>
+
+    <!-- HEADER/NAV BAR -->
+    <v-toolbar class="text-white bg-black rounded-xl mx-1 mx-lg-4 w-auto">
+      <v-toolbar-title class="text-h4 pb-3"><span class="text-subtitle-2">Your Dashboard</span><br/>Hi, {{ role?.toUpperCase() }}</v-toolbar-title>
+      <div class="d-flex align-center ga-4">
+        <template v-if="!display.mobile.value">
+          <div class="d-flex flex-column align-end text-body-2"
+            v-if="role==='student'">
+            <p>{{ studentData?.name }}</p>              
+            <p>{{ studentData?.username }}</p>              
+          </div>
+          <div class="d-flex flex-column align-end text-body-2"
+            v-else>
+            <p>{{ adminData?.name }}</p>              
+            <p>{{ adminData?.username }}</p>              
+          </div>              
+        </template>
+
+        <v-menu
+          transition="slide-y-transition"
+          :width="150"
+        >
+          <template v-slot:activator="{props}">
+            <v-icon
+              size="x-large"
+              class="mr-4"
+              color="white"
+              icon="mdi-account-circle" 
+              v-bind="props"  
+            ></v-icon>
+          </template>
+          <v-list
+          class="text-center">
+            <v-list-item
+              title="Home"
+              value="home"
+              @click="goHome"  
+            >
+            </v-list-item>
+            <v-list-item
+              title="Logout"
+              value="Logout"
+              @click="Logout"  
+            >
+            </v-list-item>
+          </v-list>
+        </v-menu>          
+
+      </div>
+
+    </v-toolbar>
+
     <!-- LEFT SIDEBAR -->
      <template v-if="display.mobile.value">
-      <v-navigation-drawer 
-        temporary
-        class="ma-4 pa-0 rounded-xl " 
-        width="300" 
-        color="uni-gold"
-      >
-        <v-list v-if="role==='admin'" lines="three">
-          <v-list-item class="font-weight-bold">Announcements</v-list-item>
+      <v-container class="pb-0">
+
+        <!--Student -->
+        <v-list v-if="role==='student'" lines="three" class="bg-uni-gold rounded-lg" height="200" elevation="6">
+          <v-list-item class="font-weight-bold announcement-title">Announcements</v-list-item>
           <v-list-item
             v-for="item in data.announcements" 
             :key="item.title"
@@ -49,18 +101,17 @@
             :subtitle="item.description"
           ></v-list-item>
         </v-list>
+        <!-- Admin -->
+         <v-select v-else
+           width="fit-content"
+           v-model="adminMenuModel"
+           chips
+           variant="plain"
+           :items="adminMenuTitles"
+         >
+         </v-select>        
 
-        <v-list v-else lines="three">
-          <v-list-item class="font-weight-bold">Announcements</v-list-item>
-          <v-list-item
-            v-for="item in data.announcements" 
-            :key="item.title"
-            :title="item.title"
-            :subtitle="item.description"
-          ></v-list-item>
-        </v-list>
-
-      </v-navigation-drawer>
+      </v-container>
      </template>
      <template v-else>
       <v-navigation-drawer 
@@ -92,62 +143,11 @@
       </v-navigation-drawer>
      </template>
 
-      <!-- HEADER/NAV BAR -->
-       <v-toolbar class="text-white bg-black rounded-xl mx-1 mx-lg-4 w-auto">
-          <v-toolbar-title class="text-h4 pb-3"><span class="text-subtitle-2">Your Dashboard</span><br/>Hi, {{ role?.toUpperCase() }}</v-toolbar-title>
-          <div class="d-flex align-center ga-4">
-            <template v-if="!display.mobile.value">
-              <div class="d-flex flex-column align-end text-body-2"
-                v-if="role==='student'">
-                <p>{{ studentData?.name }}</p>              
-                <p>{{ studentData?.username }}</p>              
-              </div>
-              <div class="d-flex flex-column align-end text-body-2"
-                v-else>
-                <p>{{ adminData?.name }}</p>              
-                <p>{{ adminData?.username }}</p>              
-              </div>              
-            </template>
-
-            <v-menu
-              transition="slide-y-transition"
-              :width="150"
-            >
-              <template v-slot:activator="{props}">
-                <v-icon
-                  size="x-large"
-                  class="mr-4"
-                  color="white"
-                  icon="mdi-account-circle" 
-                  v-bind="props"  
-                ></v-icon>
-              </template>
-              <v-list
-              class="text-center">
-                <v-list-item
-                  title="Home"
-                  value="home"
-                  @click="goHome"  
-                >
-                </v-list-item>
-                <v-list-item
-                  title="Logout"
-                  value="Logout"
-                  @click="Logout"  
-                >
-                </v-list-item>
-              </v-list>
-            </v-menu>          
-
-          </div>
-
-       </v-toolbar>
-
       <!-- MAIN CONTENT AREA -->
-      <v-container fluid class="mt-8">
+      <v-container fluid>
         <v-row>
           <v-col v-if="role==='student'" cols="12">
-            <h1 class="logged-title-font pl-5">Your Bookmarks</h1>
+            <h1 class="logged-title-font pl-5 pt-4">Your Bookmarks</h1>
           </v-col>
           <v-col 
             v-for="item in savedItems" 
@@ -172,6 +172,10 @@
 <style lang="scss" scoped>
   .logged-title-font {
     font-family:'Times New Roman', Times, serif;
+    text-decoration: underline;
+  }
+  .announcement-title {
+    min-height: 50px !important;
     text-decoration: underline;
   }
 </style>
